483c37044a8182575696e62d4061546d3ce47ec1
xin3 <39769570+xin3@users.noreply.github.com>
2020-01-07 17:39:12 -0500

    feat: config start script

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,15 +8,43 @@ process.env.NODE_ENV = 'development';
 module.exports = {
   mode: 'development',
   target: 'web',
-  devTool: 'cheap-module-source-map',
+  devtool: 'cheap-module-source-map',
   entry: './src/index',
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
     filename: 'bundle.js'
+  },
+  devServer: {
+    stats: 'minimal',
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: 'src/favicon.ico'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /(\.css)$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 };
 
-// webpack doesn't create output dir under dev mode, but it serves the app from memory, setting output path value let webpack know where to serve form the memory
+// webpack doesn't create output dir under dev mode, but it serves the app from memory, setting output path value let webpack know where to serve form the memory (a ref)
 
 // __dirname gives us currenty directory name
+// publicPath specifies the public directory of the output when it's referenced in the browser
