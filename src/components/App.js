c3f116f55f7e87ce8d5af4242537995ae93a9877
wangmta <wangmta@gmail.com>
2020-01-08 12:25:38 -0500

    feat: set up switch router

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './home/HomePage';
+import AboutPage from './about/AboutPage';
+import Header from './common/Header';
+import PageNotFound from './404';
+import CoursesPage from './courses/CoursesPage';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+
+// by declaring Header above the router, the Header will always render
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+// define entry point of the app
+import { BrowserRouter } from 'react-router-dom';
+// css is handled by webpack, it will bundle bootstrap into a single css and inject it into Index.html
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App';
 
-function Hi() {
-  debugger;
-  return <p>Hi.</p>;
-}
-
-render(<Hi />, document.getElementById('app'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app')
+);
